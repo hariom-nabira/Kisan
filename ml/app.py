8a9e074074ea8a20f90d8ad6ba2d8d9bb9ab4021
Harsh <harshstudy29@gmail.com>
2024-11-30 01:06:02 +0530

    port fix python

--- ml/app.py
+++ ml/app.py
@@ -176,4 +176,5 @@ def get_crop_info(crop_name):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
